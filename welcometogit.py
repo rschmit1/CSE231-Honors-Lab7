fd88da23e1ef703fc82772d0036a16c56958041e
guptane6 <guptane6@msu.edu>
2015-02-25 00:56:51 -0500

    Edited missing '\' in str

--- welcometogit.py
+++ welcometogit.py
@@ -1,7 +1,7 @@
 print("Hello! Congratulations on managing to fork this code successfully! \
-Make a change, and add a print statement with your name. Tell everyone you
+Make a change, and add a print statement with your name. Tell everyone you \
 managed to make it here! Then, follow \
-the instructions to commit the change to the repository. If there are any \
-conflicts, follow the steps to resolve them.")
+the instructions to commit the change to your repository. And continue \
+with the rest of the tutorial.")
 
 print("Neha Gupta (aka the TA) was here")
