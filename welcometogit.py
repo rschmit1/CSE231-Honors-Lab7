7b3c9af7fb9a443c89efa35d732cab09eabd8057
Neha <guptane6@msu.edu>
2015-02-24 20:47:23 -0500

    Neha Added Inital File

---
+++ welcometogit.py
@@ -0,0 +1,7 @@
+print("Hello! Congratulations on managing to fork this code successfully! \
+Make a change, and add a print statement with your name. Tell everyone you
+managed to make it here! Then, follow \
+the instructions to commit the change to the repository. If there are any \
+conflicts, follow the steps to resolve them.")
+
+print("Neha Gupta (aka the TA) was here")
